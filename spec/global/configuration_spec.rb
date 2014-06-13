41766e9b2e1c603a8c565c2d08467f660dde6039
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-06-13 18:46:35 +0300

    new rspec 3.0

--- spec/global/configuration_spec.rb
+++ spec/global/configuration_spec.rb
@@ -7,25 +7,25 @@ describe Global::Configuration do
   describe "#hash" do
     subject{ configuration.hash }
 
-    it{ should == hash }
+    it{ is_expected.to eq(hash) }
   end
 
   describe "#to_hash" do
     subject{ configuration.to_hash }
 
-    it{ should == hash }
+    it{ is_expected.to eq(hash) }
   end
 
   describe "key?" do
     subject{ configuration.key?(:key) }
 
-    it{ should be_true }
+    it{ is_expected.to be_truthy }
   end
 
   describe "#[]" do
     subject{ configuration[:key] }
 
-    it{ should == "value" }
+    it{ is_expected.to eq("value") }
   end
 
   describe "#[]=" do
@@ -33,32 +33,32 @@ describe Global::Configuration do
 
     before{ configuration[:new_key] = "new_value" }
 
-    it{ should == "new_value" }
+    it{ is_expected.to eq("new_value") }
   end
 
   describe "#inspect" do
     subject{ configuration.inspect }
 
-    it{ should == hash.inspect }
+    it{ is_expected.to eq(hash.inspect) }
   end
 
   describe "#method_missing" do
     context "when key exist" do
       subject{ configuration.key }
 
-      it{ should == "value" }
+      it{ is_expected.to eq("value") }
     end
 
     context "when key does not exist" do
       subject{ configuration.some_key }
 
-      it{ lambda { subject }.should raise_error(NoMethodError) }
+      it{ expect { subject }.to raise_error(NoMethodError) }
     end
 
     context "with nested hash" do
       subject{ configuration.nested.key }
 
-      it{ should == "value" }
+      it{ is_expected.to eq("value") }
     end
   end
 end
