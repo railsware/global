8a2afa107e6ef5e060a237299e4a488c11952b39
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-06-14 12:09:59 +0300

    test custom namespace

--- spec/global/global_js_spec.rb
+++ spec/global/global_js_spec.rb
@@ -2,24 +2,43 @@ require "spec_helper"
 
 describe Global, "generate js in Rails"  do
 
-  before(:each) do
-    described_class.configure do |config|
-      config.environment = "test"
-      config.config_directory = File.join(Dir.pwd, "spec/files")
+  context 'simple generate' do
+    before do
+      described_class.configure do |config|
+        config.environment = "test"
+        config.config_directory = File.join(Dir.pwd, "spec/files")
+      end
+      evaljs(described_class.generate_js)
     end
-    evaljs(described_class.generate_js({}))
+
+    it "should generate valid global config" do
+      expect(evaljs("Global.rspec_config.default_value")).to eq('default value')
+    end
+
+    it "should generate valid global config for array" do
+      expect(evaljs("Global.nested_config.some_array_value[0]")).to eq("First")
+    end
+
+    it "should generate valid global config for array" do
+      expect(evaljs("Global.nested_config.some_array_value[2]")).to eq("Third")
+    end
+
   end
 
-  it "should generate valid global config" do
-    expect(evaljs("Global.rspec_config.default_value")).to eq('default value')
-  end
+  context 'custom namespace' do
+    before do
+      described_class.configure do |config|
+        config.environment = "test"
+        config.config_directory = File.join(Dir.pwd, "spec/files")
+        config.js_namespace = 'CustomGlobal'
+      end
+      evaljs(described_class.generate_js)
+    end
 
-  it "should generate valid global config for array" do
-    expect(evaljs("Global.nested_config.some_array_value[0]")).to eq("First")
-  end
+    it "should generate valid global config" do
+      expect(evaljs("CustomGlobal.rspec_config.default_value")).to eq('default value')
+    end
 
-  it "should generate valid global config for array" do
-    expect(evaljs("Global.nested_config.some_array_value[2]")).to eq("Third")
   end
 
 end
