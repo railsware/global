dea7b8b551efc31c77e4f0a8fbb04213dcd7d029
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-07-09 14:56:12 +0300

    js cache depend on configs

--- lib/global/engine.rb
+++ lib/global/engine.rb
@@ -7,10 +7,8 @@ module Global
     initializer 'global-js.dependent_on_configs', after: "sprockets.environment" do
       if Rails.application.assets.respond_to?(:register_preprocessor)
         configs = Dir.glob("#{Global.config_directory}#{File::SEPARATOR}*.yml")
-        Rails.application.assets.register_preprocessor 'application/javascript', :'global-js_dependent_on_configs' do |ctx,data|
-          if ctx.logical_path == GLOBAL_JS_ASSET
-            configs.map{ |config| ctx.depend_on(config) }
-          end
+        Rails.application.assets.register_preprocessor 'application/javascript', :'global-js_dependent_on_configs' do |ctx, data|
+          configs.map{ |config| ctx.depend_on(config) } if ctx.logical_path == GLOBAL_JS_ASSET
           data
         end
       end
