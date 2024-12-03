c6c1a11b670f7e653d31d29a53717090238d5415
Ivan Yurchanka <vanya.yu@gmail.com>
2024-12-03 20:19:24 +0100

    replaced some doubles with instance doubles

--- spec/global/backend/gcp_secret_manager_spec.rb
+++ spec/global/backend/gcp_secret_manager_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'google/cloud/secret_manager'
+require 'google/cloud/secret_manager/v1'
 require 'global/backend/gcp_secret_manager'
 
 RSpec.describe Global::Backend::GcpSecretManager do
@@ -8,7 +9,7 @@ RSpec.describe Global::Backend::GcpSecretManager do
     described_class.new(prefix: 'prod-myapp-', client: client, project_id: 'example')
   end
 
-  let(:client) { double }
+  let(:client) { instance_double(Google::Cloud::SecretManager::V1::SecretManagerService::Client) }
 
   before do
     # rubocop:disable RSpec/VerifiedDoubles
@@ -18,15 +19,15 @@ RSpec.describe Global::Backend::GcpSecretManager do
     secret_data = double(data: 'secret value')
     secret_version_response = double(payload: secret_data)
 
-    list = double
-    allow(list).to receive(:next_page_token).and_return('')
-    allow(list).to receive(:each).and_yield(match_item).and_yield(not_match_item)
+    page = instance_double(Gapic::PagedEnumerable)
+    allow(page).to receive(:next_page_token).and_return('')
+    allow(page).to receive(:each).and_yield(match_item).and_yield(not_match_item)
 
     allow(client).to receive(:secret_version_path)
       .with(project: 'example', secret: 'prod-myapp-example-test_key', secret_version: 'latest')
       .and_return('some_key_path')
     allow(client).to receive(:access_secret_version).with(name: 'some_key_path').and_return(secret_version_response)
-    allow(client).to receive_messages(project_path: 'projects/example', list_secrets: list)
+    allow(client).to receive_messages(project_path: 'projects/example', list_secrets: page)
     # rubocop:enable RSpec/VerifiedDoubles
   end
 
